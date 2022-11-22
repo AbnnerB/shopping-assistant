d6dcd380eafda1a102fd5007d2867a4611241f93
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2022-11-22 19:32:42 -0300

    adicionei states nos inputs, e efetuei a função de soma

--- src/pages/Count/index.js
+++ src/pages/Count/index.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
 import Header from "../../components/Header";
 
 import "./count.css";
 
 export default function Count() {
+  const [valor, setValor] = useState("");
+  const [quantidade, setQuantidade] = useState(1);
+  const [total, setTotal] = useState("");
+
+  useEffect(() => {
+    function soma() {
+      if (quantidade < 1) {
+        setQuantidade(1);
+      }
+
+      let somaValores = valor * quantidade;
+      setTotal(somaValores);
+    }
+    soma();
+  }, [quantidade, valor]);
+
   return (
     <div className="containerHome">
       <Header />
@@ -18,22 +36,80 @@ export default function Count() {
         </thead>
         <tbody>
           <tr>
-            <td>Body1 linha1</td>
+            <td>nutella </td>
             <td>
-              <input className="inputTabela" type="number" />
+              <input
+                className="inputTabela"
+                type="number"
+                placeholder="00"
+                min="1"
+                value={valor}
+                onChange={(e) => setValor(e.target.value)}
+              />
             </td>
-            <td>Body3 linha1</td>
+            <td>
+              <input
+                className="inputTabela"
+                type="number"
+                placeholder="01"
+                min="1"
+                value={quantidade}
+                onChange={(e) => setQuantidade(e.target.value)}
+              />
+            </td>
           </tr>
           <tr>
-            <td>Body1 linha2</td>
-            <td>Body2 linha2</td>
-            <td>Body3 linha2</td>
+            <td>nutella 2</td>
+            <td>
+              <input
+                className="inputTabela"
+                type="number"
+                placeholder="00"
+                min="1"
+                value={valor}
+                onChange={(e) => setValor(e.target.value)}
+              />
+            </td>
+            <td>
+              <input
+                className="inputTabela"
+                type="number"
+                placeholder="01"
+                min="1"
+                value={quantidade}
+                onChange={(e) => setQuantidade(e.target.value)}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Salmão</td>
+            <td>
+              <input
+                className="inputTabela"
+                type="number"
+                placeholder="00"
+                min="1"
+              />
+            </td>
+            <td>
+              <input
+                className="inputTabela"
+                type="number"
+                min="1"
+                placeholder="01"
+              />
+            </td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
             <td>Total</td>
-            <td colSpan="2"></td>
+            <td colSpan="2">
+              {total.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </td>
           </tr>
         </tfoot>
       </table>
