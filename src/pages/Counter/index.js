00851695afa2b46fb85e2f8693dffd2dd85bb8e3
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2022-11-27 15:16:51 -0300

    criando o layout da pag counter

--- src/pages/Counter/index.js
+++ src/pages/Counter/index.js
@@ -1,23 +1,105 @@
+import { useState } from "react";
+
 import "./styles.css";
 
+import { AiFillDelete } from "react-icons/ai";
+
 export default function Counter() {
+  const [products, setProducts] = useState("");
+  const [values, setValues] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  const [arrayLine, setArrayLine] = useState([]);
+
+  function addLine() {
+    if (products.length < 1 || values.length < 1 || quantity.length < 0) {
+      alert("Digite um Valor");
+    }
+
+    const lineObj = {
+      product: products,
+      values: values,
+      quantity: quantity,
+    };
+
+    setArrayLine([...arrayLine, lineObj]);
+  }
+
   return (
     <div>
       <h3>Vamos ver se agora vai</h3>
 
       <section className="topoInputs">
         <div className="containerLabelInputs">
-          <input type="text" name="inputName" id="" />
-          <label>Produtos</label>
+          <label>Produtos:</label>
+          <input
+            type="text"
+            name="inputName"
+            placeholder="Digite..."
+            value={products}
+            onChange={(e) => setProducts(e.target.value)}
+          />
         </div>
-        <div>
-          <input type="number" name="input" id="" />
-          <label>Valor</label>
+        <div className="containerLabelInputs">
+          <label>Valor:</label>
+          <input
+            type="number"
+            name="inputValue"
+            placeholder="Digite um valor"
+            value={values}
+            onChange={(e) => setValues(e.target.value)}
+          />
         </div>
-        <div>
-          <input type="number" name="input" id="" />
-          <label>Quantidade</label>
+        <div className="containerLabelInputs">
+          <label>Quantidade:</label>
+          <input
+            type="number"
+            name="inputQuant"
+            placeholder="Digite um valor"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
         </div>
+
+        <button onClick={addLine}>Add</button>
+      </section>
+      <section className="sectionTable">
+        {arrayLine.length >= 1 && (
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Valor</th>
+                <th>Quantidade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {arrayLine.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.product} </td>
+                  <td>{item.values}</td>
+                  <td>{item.quantity}</td>
+                  <td className="tdButton">
+                    <button>
+                      <AiFillDelete />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Total</td>
+                <td colSpan="2">
+                  {/* {total.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                })} */}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        )}
       </section>
     </div>
   );
